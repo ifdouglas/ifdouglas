fc0c18f15932e07372fd8a2616c1c1ce52f1bb2f
Douglas Fernandes <ifdouglas@icloud.com>
2023-08-14 14:29:48 -0300

    feat: adding adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,21 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/styles/global.theme";
 import "../utils/i18n";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <meta name="description" content="Ready to build your idea" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8007784980150474"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
